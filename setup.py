55b0c8ddda39668cf921740527724d29b797b2e0
choROPeNt <christian.duereth@tu-dresden.de>
2024-01-19 15:08:37 +0000

    clean up, added pynrrd

--- setup.py
+++ setup.py
@@ -3,5 +3,16 @@ from setuptools import setup
 setup(
     name="torch3dseg",
     py_modules=["torch3dseg"],
-    install_requires=["torch","torchvision","numpy", "tqdm","h5py","pyyaml","tensorboard","scikit-image","torchsummary"],
+    install_requires=[
+        "torch",
+        "torchvision",
+        "numpy", 
+        "tqdm",
+        "h5py",
+        "pyyaml",
+        "tensorboard",
+        "scikit-image",
+        "torchsummary",
+        "nrrd"
+        ],
 )
