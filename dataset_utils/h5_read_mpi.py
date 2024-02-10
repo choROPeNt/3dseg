67f556c7832fc942f9a38684b1cd8c03c9153acc
choROPeNt <christian.duereth@tu-dresden.de>
2024-02-10 15:39:18 +0100

    moved in archiv

--- dataset_utils/h5_read_mpi.py
+++ dataset_utils/h5_read_mpi.py
@@ -1,41 +1,32 @@
-# from mpi4py import MPI
-# import h5py as h5
-# import os
-# import numpy as np
-
-# rank = MPI.COMM_WORLD.rank
-
-# print(rank)
-# pred_file = "/Volumes/LaCie/scratch/160_10-layer/volumes/00/160_10-layer_00_rot.h5"
-
-# pred = {}
-# with h5.File(os.path.join(pred_file),"r",driver='mpio', comm=MPI.COMM_WORLD) as f:
-#     # print(f.keys())
-#     for key in f.keys():
-#         # print(key)
-#         if key == "raw":
-#             pred[key] = f[key][:]
-#             # print(f[key].shape)
-#             # print(f[key].nbytes/1024**2)
-#             # print(f[key].dtype)
-
-# print(pred["raw"].shape)
-# print(pred["raw"].nbytes)
-# print(pred["raw"].dtype)
-
-from mpi4py import MPI
-import h5py
 import numpy as np
+import os
+import h5py
+from mpi4py import MPI
 
 
 
-rank = MPI.COMM_WORLD.rank  # The process ID (integer 0-3 for 4-process run)
+def main(file,key="predictions",**kwargs):
+    
+    assert os.path.exists(file)
 
-f = h5py.File('parallel_test.hdf5', 'w', driver='mpio', comm=MPI.COMM_WORLD)
 
-dset = f.create_dataset('test', (4,1000, 1000, 1000), dtype='i',
-                        # chunks=(1,1000, 1000, 1000), 
-                        compression="gzip")
-dset[rank] = np.full((1000, 1000, 1000), rank)
+    with h5py.File(file,"r+",driver='mpio', comm=MPI.COMM_WORLD) as f:
+        ## load data
+        data = np.argmax(f[key][:],axis=0)
+        if f["labels"]:
+            print("deleting old labels")
+            del f["labels"]
+   
+        f.create_dataset("labels",data=data.astype(np.uint8))
 
-f.close()
+    
+    
+
+
+
+
+
+if __name__=="__main__":
+
+    file = "/Volumes/data/BIIAX_model_00-lowres-2024-02-07/160_10-layer_00_rot_0000-0230_0230-0460_predictions-test.h5"
+    main(file)
