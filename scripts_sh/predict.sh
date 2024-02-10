2efd9e44cb48ccd8e9c132b8d8834b5cd39bffaa
choROPeNt <christian.duereth@tu-dresden.de>
2024-02-10 16:24:31 +0100

    adjustments for 128x128x128 model

--- scripts_sh/predict.sh
+++ scripts_sh/predict.sh
@@ -5,15 +5,15 @@
 ##################################################################
 ## SLURM Defintions
 ##################################################################
-#SBATCH --time=4:00:00                                 # walltime
+#SBATCH --time=5:00:00                                 # walltime
 #SBATCH --nodes=1                                       # number of nodes
 #SBATCH --ntasks=1                                      # limit to one node
-#SBATCH --cpus-per-task=8                               # number of processor cores (i.e. threads)
+#SBATCH --cpus-per-task=12                               # number of processor cores (i.e. threads)
 #SBATCH --partition=alpha
-#SBATCH --mem-per-cpu=8G                             # memory per CPU core
-#SBATCH --gres=gpu:1                                    # number of gpus
-#SBATCH -J "3dseg-predict"                           # job name
-#SBATCH --output=slurm_out/3dseg-predict-%j.out
+#SBATCH --mem-per-cpu=16G                             # memory per CPU core
+#SBATCH --gres=gpu:2                                    # number of gpus
+#SBATCH -J "3dseg-torch_predict"                           # job name
+#SBATCH --output=/lustre/ssd/ws/dchristi-3dseg/slurm_out/slurm_out/3dseg-torch_predict-%j.out
 #SBATCH --mail-user=christian.duereth@tu-dresden.de     # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH -A p_autoshear
