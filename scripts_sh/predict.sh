113b7d683e81bb9840f4b7e383d7fb396404aa71
choROPeNt <christian.duereth@tu-dresden.de>
2024-02-16 13:20:34 +0100

    minor changes

--- scripts_sh/predict.sh
+++ scripts_sh/predict.sh
@@ -5,15 +5,15 @@
 ##################################################################
 ## SLURM Defintions
 ##################################################################
-#SBATCH --time=5:00:00                                 # walltime
+#SBATCH --time=10:00:00                                 # walltime
 #SBATCH --nodes=1                                       # number of nodes
 #SBATCH --ntasks=1                                      # limit to one node
-#SBATCH --cpus-per-task=12                               # number of processor cores (i.e. threads)
+#SBATCH --cpus-per-task=24                               # number of processor cores (i.e. threads)
 #SBATCH --partition=alpha
 #SBATCH --mem-per-cpu=16G                             # memory per CPU core
-#SBATCH --gres=gpu:2                                    # number of gpus
+#SBATCH --gres=gpu:4                                    # number of gpus
 #SBATCH -J "3dseg-torch_predict"                           # job name
-#SBATCH --output=/lustre/ssd/ws/dchristi-3dseg/slurm_out/slurm_out/3dseg-torch_predict-%j.out
+#SBATCH --output=/lustre/ssd/ws/dchristi-3dseg/slurm_out/3dseg-torch_predict-%j.out
 #SBATCH --mail-user=christian.duereth@tu-dresden.de     # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH -A p_autoshear
--- torch3dseg/utils/predictor.py
+++ torch3dseg/utils/predictor.py
@@ -197,6 +197,10 @@ class StandardPredictor(_AbstractPredictor):
                 logger.info(f'Dataset loaded with mirror padding: {dataset.mirror_padding}. Cropping before saving...')
 
                 prediction_map = prediction_map[:, z_s, y_s, x_s]
+            
+            ##TODO set out put to labels
+            if self.config['predictor']['output_type'] == 'classes':
+                prediction_map = np.argmax(prediction_map,axis=0).astype(np.uint8)
 
             output_file.create_dataset(prediction_dataset, data=prediction_map, compression="gzip")
 
