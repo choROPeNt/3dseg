5c5d28dd2d330426a9701394604d8353ddd78856
choROPeNt <christian.duereth@tu-dresden.de>
2024-05-24 12:00:04 +0200

    created script for data conversion

--- dataset_utils/h5_to_nrrd.py
+++ dataset_utils/h5_to_nrrd.py
@@ -1,58 +1,164 @@
 import os
-import h5py
+import h5py as h5
 import nrrd
 import argparse
 import numpy as np
-import time as time
-from tqdm import tqdm
-
+from skimage.measure import label
 import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'cividis'
 
-def main(file_pred,file_seg):
-    _ , header_seg = nrrd.read(file_seg)
-    pred_file = h5py.File(file_pred, 'r')
-    c,d,h,w = pred_file['predictions'].shape
 
-    print(pred_file['predictions'].dtype,pred_file['predictions'].shape)
+def readH5(file_path,**kwargs):
+    """
+    generic function to read a h5 file with one 
+    subgroup elements by given filepath in a Dictonary 
+    """
+    
+    out = {}
 
-    out_arr = np.empty((w,h,d),dtype=np.ushort)
-    print('output file memory consumption: %.3f  Gbyte' %(out_arr.nbytes/1024**3))
-    ## 
+    with h5.File(file_path, 'r') as f:
 
-    ##TODO implement np.argmax(array,dim=0) !!!!!!
-    for slice in tqdm(range(d)):
-        ## load patch
-        pred_arr = np.array(pred_file['predictions'][:,slice,:,:])
+        for key in f.keys():
+
+            if isinstance(f[key],h5.Group):
+                out[key] = {}
+                for sub_key in f[key]:
+                    out[key][sub_key] = f[key][sub_key][...]
+            else:
+                out[key] = f[key][:]
+        f.close()
+    return out
+
+
+def create_nrrd_header(size, spacing,space_origin, data_type='float', space='left-posterior-superior'):
+    header = {
+        'type': data_type,
+        'dimension': len(size),
+        'space dimension': len(size),
+        'sizes': size,
+        'space': space,
+        'space origin': space_origin
+    }
+
+    if len(spacing) == len(size):
+        header['space directions'] = np.diag(spacing).tolist()
+        header['kinds'] = ['domain'] * len(size)
+
+    header.update({
+        'endian': 'little',
+        'encoding': 'raw',
+    })
+
+    return header
+
+def numpy_dtype_to_nrrd_dtype(dtype):
+    """
+    Convert NumPy dtype to NRRD dtype string.
+
+    Parameters:
+    - dtype: NumPy dtype object
+
+    Returns:
+    - NRRD dtype string
+    """
+    dtype_mapping = {
+        np.uint8: 'uint8',
+        np.uint16: 'uint16',
+        np.uint32: 'uint32',
+        np.uint64: 'uint64',
+        np.int8: 'int8',
+        np.int16: 'int16',
+        np.int32: 'int32',
+        np.int64: 'int64',
+        np.float16: 'float16',
+        np.float32: 'float32',
+        np.float64: 'float64',
+    }
+
+    return dtype_mapping.get(dtype, 'unknown')
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Process a file path.")
+
+    # Add the argument
+    parser.add_argument(
+        'file_path', 
+        type=str, 
+        help='The path to the file to be processed'
+    )
+
+     # Parse the arguments
+    args = parser.parse_args()
+    # Extract the directory path and file name
+    directory = os.path.dirname(args.file_path)
+    file_name = os.path.basename(args.file_path)
+    file_name_ = file_name.split('.')
+
+    # Print the directory and file name
+    print(f'Directory path: {directory}')
+    print(f'File name: {file_name}')
+    print(f'File name: {file_name_}')
+
+    file   = readH5(os.path.join(directory,file_name))
+
+    data= {}
+
+    for key, item in file.items():
+  
+        if key == "predictions":
+            print(f"proccessing {key} with {item.shape} and {item.dtype}")
+            obj_ids = np.unique(item)[1:] # skip background
+            print(obj_ids)
+            
+            
+            masks = item == obj_ids[:, None, None, None]
+            labels = np.zeros((len(obj_ids),*item.shape)).astype(np.int16)
+
+            slice = 30
+
+            for cls in range(0,masks.shape[0]):
+                labels[cls-1] = label(masks[cls,::],connectivity=2)
+                plt.imshow(labels[cls-1,:,:,slice])
+                plt.show()
+
+            
+            print(f"created new array labels with {labels.shape} and {labels.dtype}")
+
+
+            data_type_seg = labels.dtype
+            size = labels.shape
+
+            spacing = (1,1,1)
+            space_origin = (0 , 0 , 0 )
+
+            header_seg = create_nrrd_header(size, spacing, space_origin, data_type_seg)
+            for i, array in enumerate(labels):
+                fileout = file_name_[0] +f".{i}" +".pred.nrrd"
+                print(f"Creating file {fileout} in {directory}")
+    
+                nrrd.write(os.path.join(directory,fileout), labels[i,::],header=header_seg)
         
-        pred_arr_out = np.empty((h,w))
+        
+        if key == "raw":
+            print(f"proccessing {key} with {item.shape} and {item.dtype}")
+            data_type_seg = item.dtype
+            size = item.shape
 
-        for cls in range(c):
-            pred_arr[cls,:,:] = np.where(pred_arr[cls,:,:] >= .5, cls, 0)
-            pred_arr_out = np.where(pred_arr[cls,:,:] >= .5, cls, pred_arr_out).astype(np.ushort)
+            spacing = (1,1,1)
+            space_origin = (0 , 0 , 0 )
 
-        out_arr[:,:,slice] = pred_arr_out.transpose()
-
-        # fig, axs = plt.subplots(2,2)
-        # axs[0][0].imshow(pred_arr_out)
-        # axs[0][1].imshow(pred_arr[1,:,:])
-        # axs[1][0].imshow(pred_arr[2,:,:])
-        # axs[1][1].imshow(pred_arr[3,:,:])
-
-        plt.show()
-
-    ##TODO change this here no hard coding
-    header_seg['Segment0_Extent'] = '0 990 0 1012 0 993'
-    header_seg['Segment1_Extent'] = '0 990 0 1012 0 993'
-    header_seg['Segment2_Extent'] = '0 990 0 1012 0 993'
-    nrrd.write('output.nrrd', out_arr, header_seg)
-
-
+            header_seg = create_nrrd_header(size, spacing, space_origin, data_type_seg)
+            
+            fileout = file_name_[0] +".vol.nrrd"
+            print(f"Creating file {fileout} in {directory}")
+    
+            nrrd.write(os.path.join(directory,fileout), item,header=header_seg)
 
 if __name__ == "__main__":
-    file_pred   = './predict/NEAPEL/neapel_001_all_predictions.h5'
-    file_seg    = './data/NEAPEL/neapel_001/Segmentation17_05_23.nrrd'
     
-    start_time = time.time()
-    main(file_pred,file_seg)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    main()
+    
     
