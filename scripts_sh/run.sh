8e550e685bc4ba4848bf220c37363d876c35eaa2
choROPeNt <christian.duereth@tu-dresden.de>
2025-02-15 14:30:10 +0100

    minor updates

--- scripts_sh/run.sh
+++ scripts_sh/run.sh
@@ -5,18 +5,18 @@
 ##################################################################
 ## SLURM Defintions
 ##################################################################
-#SBATCH --time=5:00:00                                 # walltime
+#SBATCH --time=6:00:00                                 # walltime
 #SBATCH --nodes=1                                       # number of nodes
 #SBATCH --ntasks=1                                      # limit to one node
-#SBATCH --cpus-per-task=8                               # number of processor cores (i.e. threads)
+#SBATCH --cpus-per-task=6                               # number of processor cores (i.e. threads)
 #SBATCH --partition=alpha
-#SBATCH --mem-per-cpu=8G                             # memory per CPU core
+#SBATCH --mem-per-cpu=16G                             # memory per CPU core
 #SBATCH --gres=gpu:1                                    # number of gpus
 #SBATCH -J "torch_3dseg"                           # job name
-#SBATCH --output=slurm_out/3dseg_BIIAX_lowres-%j.out
+#SBATCH --output=slurm_out/3dseg_NEAPEL_run03-%j.out
 #SBATCH --mail-user=christian.duereth@tu-dresden.de     # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
-#SBATCH -A p_autoshear
+#SBATCH -A p_biiax
 ##################################################################
 ##################################################################
 
@@ -24,14 +24,11 @@ module load release/23.04 GCC/12.2.0 Python/3.10.8 OpenMPI/4.1.4 CUDA/11.8.0
 
 source .venv_3dseg/bin/activate
 
-# train_config=./configs/BIIAX/00-lowres-3c/train.yml
-# pred_config=./configs/BIIAX/00-lowres-3c/pred.yml
-
-train_config=./configs/pore-detection/train_config_pore.yml
-
+train_config=configs/NEAPEL/model_64x64x64_bin/config_train.yml
+pred_config=configs/NEAPEL/model_64x64x64_bin/config_pred.yml
 
 python ./scripts/train.py --config $train_config
 
-# python ./scripts/predict.py --config $pred_config
+python ./scripts/predict.py --config $pred_config
 
 exit 0
