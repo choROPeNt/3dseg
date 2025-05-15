778395328c9ecb7c9af53927477f2169dce4af1c
choROPeNt <christian.duereth@tu-dresden.de>
2025-05-15 10:36:51 +0200

    small training adjsutments

--- scripts_sh/train.sh
+++ scripts_sh/train.sh
@@ -6,7 +6,7 @@
 ## SLURM Defintions
 ##################################################################
 # walltime
-#SBATCH --time=12:00:00                                  
+#SBATCH --time=5:00:00                                  
 # number of nodes
 #SBATCH --nodes=1                                       
 # number of tasks
